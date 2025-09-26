c7c9ef769deb056efeb4fa6afb4516f7403449e2
Ronak yadav <ryadav@planittesting.com>
2025-09-26 16:22:33 +0530

    Enum Functionality added for building the api path

--- src/test/java/features/login.feature
+++ src/test/java/features/login.feature
@@ -1,12 +1,16 @@
 Feature: Validating Place API's
 
-Scenario: Verify if the Place is being added successfully using AddPlaceAPI
-  Given Add Place Payload "single" "single test address" "demo"
-  When user calls "AddPlaceAPI" with "POST" request
-  Then the API call is success with status code "200"
-  And "status" in response body is "OK"
-  And "scope" in response body is "APP"
-
+#Scenario: Verify if the Place is being added successfully using AddPlaceAPI
+#  Given Add Place Payload "single" "single test address" "demo"
+#  When user calls "AddPlaceAPI" with "POST" request
+#  Then the API call is success with status code "200"
+#  And "status" in response body is "OK"
+#  And "scope" in response body is "APP"
+#
+#Scenario: Verify if the Place is being deleting successfully using AddPlaceAPI
+#    Given Add Place Payload "single" "single test address" "demo"
+#    When user calls "DeletePlaceAPI" with "POST" request
+#    Then the API call is success with status code "200"
 
 #  Can be reused using parameterization concept within one single method
 #  When user calls "DeletePlaceAPI" with POST request
@@ -18,6 +22,8 @@ Scenario: Verify if the Place is being added successfully using AddPlaceAPI
     Then the API call is success with status code "200"
     And "status" in response body is "OK"
     And "scope" in response body is "APP"
+    And verify place_Id created maps to the "<name>" using "GetPlaceAPI"
+
     Examples:
     | name  | address     | language  |
     | ronak | Nallasopara | Hindi     |
--- src/test/java/stepDefinations/loginStepDef.java
+++ src/test/java/stepDefinations/loginStepDef.java
@@ -18,9 +18,11 @@ import java.io.IOException;
 
 public class loginStepDef extends testBaseUtility {
     Response resOfAddPlace;
+    Response resOfGetPlace;
     Payload body;
     testDataBuild dataBuilder = new testDataBuild();
     RequestSpecification reqSpec;
+    String placeId;
 
     @Given("Add Place Payload {string} {string} {string}")
     public void Add_Place_Payload(String name, String address, String language) throws IOException, InterruptedException {
@@ -34,9 +36,31 @@ public class loginStepDef extends testBaseUtility {
     public void user_calls_with_POST_request(String resource, String requestType) {
         APIResources apiResource = APIResources.valueOf(resource);
         String path = apiResource.getResource();
-        resOfAddPlace = reqSpec
-                .when()
-                .post(path);
+        switch(requestType.toLowerCase()) {
+            case "get" -> {
+                resOfAddPlace = reqSpec.when().get(path);
+                placeId = resOfAddPlace.jsonPath().get("place_id").toString();
+            }
+            case "post" -> {
+                resOfAddPlace = reqSpec.when().post(path);
+                placeId = resOfAddPlace.jsonPath().get("place_id").toString();
+            }
+            case "put" -> {
+                resOfAddPlace = reqSpec.when().put(path);
+                placeId = resOfAddPlace.jsonPath().get("place_id").toString();
+            }
+            case "patch" -> {
+                resOfAddPlace = reqSpec.when().patch(path);
+                placeId = resOfAddPlace.jsonPath().get("place_id").toString();
+            }
+            case "delete" -> {
+                resOfAddPlace = reqSpec.when().delete(path);
+                placeId = resOfAddPlace.jsonPath().get("place_id").toString();
+            }
+            default -> {
+                Assert.fail("No Request Specified !");
+            }
+        }
     }
 
     @Then("the API call is success with status code {string}")
@@ -48,5 +72,19 @@ public class loginStepDef extends testBaseUtility {
     public void in_response_body_is(String key, String value) {
         String resValue = resOfAddPlace.jsonPath().get(key);
         Assert.assertEquals(resValue, value);
+    }
+
+    @Then("verify place_Id created maps to the {string} using {string}")
+    public void verify_place_id_created_maps_to_the_using(String name, String resource) {
+        APIResources apiResource = APIResources.valueOf(resource);
+        String path = apiResource.getResource();
+        resOfGetPlace = given()
+                .spec(reqSpec)
+                .queryParam("place_id", placeId)
+                .queryParam("key", prop.getProperty("key"))
+                .when()
+                .get(path);
+        String resName = resOfGetPlace.jsonPath().get("name").toString();
+        Assert.assertEquals(resName, name);
     }
 }
